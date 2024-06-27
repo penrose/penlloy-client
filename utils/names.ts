32164ac43aaa1c95c4457931fc106aaefab6cd63
Yiliang Liang <liangyiliang@outlook.com>
2024-06-27 13:17:24 -0400

    Fixed PRIME indicator

--- utils/names.ts
+++ utils/names.ts
@@ -1,21 +1,29 @@
 import { AlloyInstanceAtom } from "../types/AlloyInstance.js";
 
 export const toPenroseTypeName = (typeName: string) => {
-  return `_type_${typeName.replaceAll("/", "_SLASH_")}`;
+  return `_type_${typeName
+    .replaceAll("$", "_DOLLAR_")
+    .replaceAll("/", "_SLASH_")
+    .replaceAll("'", "_PRIME_")}`;
 };
 
 export const toPenroseRelationFunctionName = (relName: string) => {
   return `_rel_${relName
     .replaceAll("$", "_DOLLAR_")
-    .replaceAll("/", "_SLASH_")}`;
+    .replaceAll("/", "_SLASH_")
+    .replaceAll("'", "_PRIME_")}`;
 };
 
 export const toPenroseSetFunctionName = (setName: string) => {
   return `_set_${setName
     .replaceAll("$", "_DOLLAR_")
-    .replaceAll("/", "_SLASH_")}`;
+    .replaceAll("/", "_SLASH_")
+    .replaceAll("'", "_PRIME_")}`;
 };
 
 export const toPenroseAtomName = ({ type, index }: AlloyInstanceAtom) => {
-  return `_atom_${type.replaceAll("/", "_SLASH_")}_${index}`;
+  return `_atom_${type
+    .replaceAll("$", "_DOLLAR_")
+    .replaceAll("/", "_SLASH_")
+    .replaceAll("'", "_PRIME_")}_${index}`;
 };
